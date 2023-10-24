bc84c432f1dde399868edc72e47f1c0584b3a9bf
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-24 10:31:01 +0200

    Change output file name in furtherVectorLengths.sh

--- hw_3/ex_1/furtherVectorLengths.sh
+++ hw_3/ex_1/furtherVectorLengths.sh
@@ -5,9 +5,9 @@ make
 
 inputLengths=(200000 400000 600000 800000 1000000 1200000 1400000 1600000 1800000 2000000)
 
-> output.txt
-echo "Writing outputs to output.txt"
+> output_vector_lengths.txt
+echo "Writing outputs to output_vector_lengths.txt"
 
 for inputLength in "${inputLengths[@]}"; do
-  ./lab3_ex1.out "$inputLength" >> output.txt
+  ./lab3_ex1.out "$inputLength" >> output_vector_lengths.txt
 done
