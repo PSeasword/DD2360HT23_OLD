e15287ba3627c58baab8afcab78b5e6776920665
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-24 10:32:59 +0200

    Changed file name in furtherMatrixSizes.sh

--- hw_3/ex_2/furtherMatrixSizes.sh
+++ hw_3/ex_2/furtherMatrixSizes.sh
@@ -7,12 +7,12 @@ inputDims1=(550 600 650 700 750 800 850)
 inputDim2=1024
 inputDim3=4150
 
-> output.txt
-echo "Writing outputs to output.txt"
+> output_matrix_sizes.txt
+echo "Writing outputs to output_matrix_sizes.txt"
 
 for inputDim1 in "${inputDims1[@]}"; do
   echo "Currently doing $inputDim1 $inputDim2 $inputDim3"
-  ./lab3_ex2.out "$inputDim1" "$inputDim2" "$inputDim3" >> output.txt
+  ./lab3_ex2.out "$inputDim1" "$inputDim2" "$inputDim3" >> output_matrix_sizes.txt
 done
 
-cat output.txt
+cat output_matrix_sizes.txt
