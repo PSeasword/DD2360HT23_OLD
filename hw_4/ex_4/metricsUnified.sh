b76a0f417a5a4f70b8ed6328a55b4f6e2325955c
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-24 11:09:35 +0200

    Changed name of nvvp file in metricsUnified.sh

--- hw_4/ex_4/metricsUnified.sh
+++ hw_4/ex_4/metricsUnified.sh
@@ -11,17 +11,18 @@ echo "Using dimX $dimX and nsteps $nsteps"
 echo "==================== With prefetching ===================="
 
 for i in {1..1}; do
-  nvprof --output-profile profileEx3WithPrefetching.nvvp -f ./lab4_ex4.out "$dimX" "$nsteps" 1
+  nvprof --output-profile withPrefetching.nvvp -f ./lab4_ex4.out "$dimX" "$nsteps" 1
   nvprof ./lab4_ex4.out "$dimX" "$nsteps" 1
   echo ""
 done
 
 echo ""
+echo ""
 
 echo "==================== Without prefetching ===================="
 
 for i in {1..1}; do
-  nvprof --output-profile profileEx3WithoutPrefetching.nvvp -f ./lab4_ex4.out "$dimX" "$nsteps" 0
+  nvprof --output-profile withoutPrefetching.nvvp -f ./lab4_ex4.out "$dimX" "$nsteps" 0
   nvprof ./lab4_ex4.out "$dimX" "$nsteps" 0
   echo ""
 done
