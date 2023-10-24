756270d4fdce04a04b0eda97871ddc96f145043c
PSeasword <148008778+PSeasword@users.noreply.github.com>
2023-10-24 10:28:29 +0200

    Update differentSegmentSizes.sh

--- hw_4/ex_2/differentSegmentSizes.sh
+++ hw_4/ex_2/differentSegmentSizes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make clean
-make lab4_ex2_pinned.out
+make lab4_ex2.out
 
 output4Streams="output_segment_4.txt"
 outputVaryingStreams="output_segment_varying.txt"
@@ -9,16 +9,16 @@ outputVaryingStreams="output_segment_varying.txt"
 inputLength=100000000
 segmentSizes=(10000 100000 1000000 10000000 20000000 40000000 60000000 80000000 100000000)
 
-echo "Running lab4_ex2_pinned.out with 4 streams, saving to $output4Streams"
+echo "Running lab4_ex2.out with 4 streams, saving to $output4Streams"
 > $output4Streams
 
 for segmentSize in "${segmentSizes[@]}"; do
-  ./lab4_ex2_pinned.out "$inputLength" "$segmentSize" 4 >> "$output4Streams"
+  ./lab4_ex2.out "$inputLength" "$segmentSize" 4 >> "$output4Streams"
 done
 
-echo "Running lab4_ex2_pinned.out with varying number of streams, saving to $outputVaryingStreams"
+echo "Running lab4_ex2.out with varying number of streams, saving to $outputVaryingStreams"
 > $outputVaryingStreams
 
 for segmentSize in "${segmentSizes[@]}"; do
-  ./lab4_ex2_pinned.out "$inputLength" "$segmentSize" - >> "$outputVaryingStreams"
+  ./lab4_ex2.out "$inputLength" "$segmentSize" - >> "$outputVaryingStreams"
 done
